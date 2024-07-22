40722f06b2ab37e9804060e382f1d885e4f99101
Naeem Nadeem <naeemnadeemlive@gmail.com>
2024-07-22 21:41:49 +0500

    Testng

--- src/test/java/Automation/test/LoginPage.java
+++ src/test/java/Automation/test/LoginPage.java
@@ -1,11 +1,7 @@
 package Automation.test;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginPage extends Base{
@@ -15,7 +11,6 @@ public class LoginPage extends Base{
     public void LoginWithValidUsernameValidPassword(){
         login = new Login(driver);
         login.login("naeemtester","naeemtester");
-
         //Verification
         String text = driver.findElement(By.className("welcome_menu")).getText();
         Assert.assertEquals(text,"Welcome to Adactin Group of Hotels");
@@ -26,6 +21,24 @@ public class LoginPage extends Base{
     public void LoginWithInvalidUsernameInvalidPassword(){
         login = new Login(driver);
         login.login("alikahna","aasds");
+        //verification
+        String text = driver.findElement(By.className("auth_error")).getText();
+        Assert.assertEquals(text,"Invalid Login details or Your Password might have expired. Click here to reset your password");
+    }
+
+    @Test(priority = 3)
+    public void LoginWithvalidUsernameInvalidPassword(){
+        login = new Login(driver);
+        login.login("naeemtester","aasds");
+        //verification
+        String text = driver.findElement(By.className("auth_error")).getText();
+        Assert.assertEquals(text,"Invalid Login details or Your Password might have expired. Click here to reset your password");
+    }
+
+    @Test(priority = 4)
+    public void LoginWithInvalidUsernamevalidPassword(){
+        login = new Login(driver);
+        login.login("11221","naeemtester");
         //verification
         String text = driver.findElement(By.className("auth_error")).getText();
         Assert.assertEquals(text,"Invalid Login details or Your Password might have expired. Click here to reset your password");
